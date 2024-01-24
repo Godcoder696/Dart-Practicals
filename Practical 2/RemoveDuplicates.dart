a9d44d22df8bc92f52e937899d8ceb3bce98e703
Godcoder696 <117354227+Godcoder696@users.noreply.github.com>
2024-01-24 11:22:37 +0530

    Duplicates

--- Practical 2/RemoveDuplicates.dart
+++ Practical 2/RemoveDuplicates.dart
@@ -1,3 +1,11 @@
 void main() {
-  var input = new List();
+  var input = ["lakshay", "geet", "lakshay", "ritesh", "Disha", "Disha"];
+  var ans = [];
+
+  for (int i = 0; i < input.length; i++) {
+    if (ans.contains(input[i])) continue;
+    ans.add(input[i]);
+  }
+
+  print("The Result is: $ans");
 }
